73298c0b1be43a796cd9063e8f1d4118238fff72
Fadhellya <muhamadfadhel50@gmail.com>
2023-01-04 09:45:11 +0700

    Initial commit

---
+++ src/main/java/com/mycompany/fadelperpustakaanclient/model/Peminjaman.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.fadelperpustakaanclient.model;
+public class Peminjaman {
+    private Long peminjamanId;
+    private Long bukuId;
+    private Long anggotaId;
+    private String tglPinjam;
+    private String tglKembali;
+}
